97f0956eded2b08d0230703351ddeb41b2cfa5fd
devviktoria <74530862+devviktoria@users.noreply.github.com>
2026-01-27 20:55:26 +0100

    CreateEmotionCountersForJoke creation

---
+++ jesterclub2026/jcdatabase/Migrations/20260127191906_JokeInsertTrigger.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace jcdatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class JokeInsertTrigger : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = @"CREATE TRIGGER dbo.CreateEmotionCountersForJoke
+ON dbo.Jokes
+AFTER INSERT
+AS
+    IF (ROWCOUNT_BIG() = 0)
+    RETURN;
+
+    DECLARE @newJokeId INT;
+    SET @newJokeId = @@IDENTITY;
+    PRINT @newJokeId
+    INSERT INTO EmotionCounters(JokeId, Emotion, Counter) VALUES (@newJokeId, 'lshic', 0);
+    INSERT INTO EmotionCounters(JokeId, Emotion, Counter) VALUES (@newJokeId, 'lol', 0);
+    INSERT INTO EmotionCounters(JokeId, Emotion, Counter) VALUES (@newJokeId, 'happy', 0);
+    INSERT INTO EmotionCounters(JokeId, Emotion, Counter) VALUES (@newJokeId, 'none', 0);
+    INSERT INTO EmotionCounters(JokeId, Emotion, Counter) VALUES (@newJokeId, 'sleepy', 0);
+";
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sql = "DROP TRIGGER dbo.CreateEmotionCountersForJoke";
+            migrationBuilder.Sql(sql);
+        }
+    }
+}
