daef536667ebaf3975ffd699f2df7af524ca1ac6
devviktoria <74530862+devviktoria@users.noreply.github.com>
2026-01-27 19:47:05 +0100

    Seed the User table via a migration

---
+++ jesterclub2026/jcdatabase/Migrations/20260127183429_UserSeed.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace jcdatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var insertSql = @"
+INSERT INTO Users (UserName, UserEmail) VALUES ('Viki', 'viki@nodomain.com');
+INSERT INTO Users (UserName, UserEmail) VALUES ('Charles', 'charles@nodomain.com');
+";
+            migrationBuilder.Sql(insertSql);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
