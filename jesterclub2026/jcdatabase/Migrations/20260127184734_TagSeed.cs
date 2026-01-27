da36ac50bcfc09096c98032345ff4e9ae11a6750
devviktoria <74530862+devviktoria@users.noreply.github.com>
2026-01-27 19:58:29 +0100

    Seed the Tag table via a migration

---
+++ jesterclub2026/jcdatabase/Migrations/20260127184734_TagSeed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace jcdatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class TagSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var insertSql = @"
+INSERT INTO Tags (Name) VALUES ('programming');
+INSERT INTO Tags (Name) VALUES ('animals');
+INSERT INTO Tags (Name) VALUES ('cat');
+INSERT INTO Tags (Name) VALUES ('dog');
+INSERT INTO Tags (Name) VALUES ('sci-fi');
+INSERT INTO Tags (Name) VALUES ('money');
+INSERT INTO Tags (Name) VALUES ('food');
+";
+            migrationBuilder.Sql(insertSql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
