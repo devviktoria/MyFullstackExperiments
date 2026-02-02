e28d9360e19fcdeada49a67db39776abc87e8e5d
devviktoria <74530862+devviktoria@users.noreply.github.com>
2026-02-02 16:39:36 +0100

    Adding CORS handeling

--- jesterclub2026/jcwebapi/Program.cs
+++ jesterclub2026/jcwebapi/Program.cs
@@ -7,10 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+const string JesterClubSpecificOrigins = "_JesterClubSpecificOrigins";
 string connString = builder.Configuration.GetConnectionString("jcwebapi") ?? "";
 builder.Services.AddSqlServerServices(connString);
 builder.Services.AddDataServices();
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: JesterClubSpecificOrigins,
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
@@ -19,6 +30,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseCors(JesterClubSpecificOrigins);
 
 app.UseHttpsRedirection();
 
